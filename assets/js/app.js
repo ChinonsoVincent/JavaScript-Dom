78a171ae6bbc5a32f786c92184559e16262b2846
Chinonso Vincent <nchinonsovincent@gmail.com>
2022-10-14 06:17:47 +0100

    creating and modifying Elements

--- assets/js/app.js
+++ assets/js/app.js
@@ -20,10 +20,43 @@ const title = document.querySelector("#main-heading");
 
 title.style.color = "red"; //just like inlne styling
 
-const listItem = document.querySelectorAll(".list-items");
+// const listItem = document.querySelectorAll(".list-items");
 
-for (i = 0; i < listItem.length; i++) {
-	listItem[i].style.fontSize = "2rem";
-}
+// for (i = 0; i < listItem.length; i++) {
+// 	listItem[i].style.fontSize = "2rem";
+// }
 
 //Creating Elements
+const ul = document.querySelector("ul");
+const li = document.createElement("li");
+
+//Adding Elements
+ul.append(li);
+
+//Modifying Text
+li.innerText = "X-Men";
+// const firstListItem = document.querySelector(".list-items");
+
+// console.log(firstListItem.innerText);
+// console.log(firstListItem.textContent);
+// console.log(firstListItem.innerHTML);
+
+// //setAttribute
+// li.setAttribute("id", "main-heading");
+
+// //Remove Attributes
+// li.removeAttribute("id");
+
+// //getAttribute
+// const title3 = document.querySelector("#main-heading");
+
+// console.log(title.getAttribute("id"));
+
+//Modifying attribute and Classes
+li.classList.add("list-items"); //add it to the class list of same class name
+li.classList.remove("list-items"); //removes the class name
+
+console.log(li.classList.contains("list-items")); //to check if an element contains an attribute
+
+//Remove Element
+li.remove();
